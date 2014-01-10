688bbc5d7f5ff79da02887f9337daa2dfee83d79
Phillip Epstein <Phillip@core-tech.com>
2014-01-10 16:27:00 -0700

    Started on animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -80,28 +80,85 @@ var animate = {
 		cords.logo.target.setAttribute('transform', 'translate(' + cords.logo.x + ',' + cords.logo.y + ')');
 	},
 
-	start: function(){
-		// var initial_ts = new Date().getTime();
-		// var duration = 2000; // this animation should last for 2 seconds
-		// var path = fetchPreparedSVGPath();
-		// var length = path.getTotalLength();
-		// var handle = 0;
-		 
-		// path.style.strokeDasharray = length + ' ' + length; 
-		// path.style.strokeDashoffset = length;
-		 
-		// var draw = function() {
-		//    var progress = (Date.now() - initial_ts)/duration;
-		//    if (progress >= 1) {
-		//      window.cancelAnimationFrame(handle);
-		//    } else {
-		//      path.style.strokeDashoffset = Math.floor(length * (1 - progress));
-		//      handle = window.requestAnimationFrame(draw);
-		//    }
-		// };
-		// draw();
-	}
+	animate: function(){
+		var that = this;
+		var cords = this.getCordinates();
+		var line1 = cords.lines.line1;
+		var line2 = cords.lines.line2;
 
-}
+		this.drawLineHoriz(
+			line1.target,
+			line1.params.x1,
+			line1.params.y1,
+			line1.params.x2,
+			line1.params.y2,
+			function(){
+				that.drawLineVert(
+					line2.target,
+					line2.params.x1,
+					line2.params.y1,
+					line2.params.x2,
+					line2.params.y2,
+					function(){
+						cords.logo.target.setAttribute('transform', 'translate(' + cords.logo.x + ',' + cords.logo.y + ')');
+					}
+				);
+			}
+		);
+	},
 
-animate.quickDraw();
+	drawLineHoriz: function(line, x1, y1, x2, y2, cb){
+		var initialTs = new Date().getTime();
+		var duration = 4000; // this animation should last for 2 seconds
+		var handle = 0;
+
+		line.setAttribute('x1', x1);
+		line.setAttribute('y1', y1);
+		line.setAttribute('y2', y2);
+		 
+		var draw = function() {
+			var progress = (Date.now() - initialTs)/duration;
+			if (progress >= 1) {
+				window.cancelAnimationFrame(handle);
+				line.setAttribute('x2', x2);
+				cb();
+			} else {
+				var calcProgress = x2 * progress;
+				var endPointPos = calcProgress <= x1 ? x1 : calcProgress;
+
+				line.setAttribute('x2', endPointPos);
+				handle = window.requestAnimationFrame(draw);
+			}
+		};
+		draw();
+	},
+
+	drawLineVert: function(line, x1, y1, x2, y2, cb){
+		var initialTs = new Date().getTime();
+		var duration = 4000; // this animation should last for 2 seconds
+		var handle = 0;
+
+		line.setAttribute('x1', x1);
+		line.setAttribute('x2', x2);
+		line.setAttribute('y1', y1);
+		 
+		var draw = function() {
+			var progress = (Date.now() - initialTs)/duration;
+			if (progress >= 1) {
+				window.cancelAnimationFrame(handle);
+				line.setAttribute('y2', y2);
+				cb();
+			} else {
+				var calcProgress = y2 * progress
+				var endPointPos = calcProgress <= y1 ? y1 : calcProgress;
+
+				line.setAttribute('y2', endPointPos);
+				handle = window.requestAnimationFrame(draw);
+			}
+		};
+		draw();
+	},
+
+};
+
+animate.animate();
