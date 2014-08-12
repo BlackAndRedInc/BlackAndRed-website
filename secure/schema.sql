6f971af1952d4880a59744a6f71d0873047e7f12
Phillip Epstein <Phillip@blackandred.io>
2014-08-12 09:33:24 -0600

    - Image Updates - Schema Updates

--- secure/schema.sql
+++ secure/schema.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.34)
 # Database: blackAndRed
-# Generation Time: 2014-08-10 20:54:32 +0000
+# Generation Time: 2014-08-12 15:25:03 +0000
 # ************************************************************
 
 
@@ -57,31 +57,32 @@ CREATE TABLE `projects` (
   `end-client-short-name` varchar(255) DEFAULT NULL,
   `end-client-name` varchar(255) DEFAULT NULL,
   `description` blob,
+  `use-short-name` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
 
-INSERT INTO `projects` (`id`, `client-id`, `name`, `end-client-short-name`, `end-client-name`, `description`)
+INSERT INTO `projects` (`id`, `client-id`, `name`, `end-client-short-name`, `end-client-name`, `description`, `use-short-name`)
 VALUES
-	(1,1,'Mane Event','vfw','VFW','For VFW we were tasked with creating a microsite fundraiser and mobile application that would be executed over Facebook messaging to spread the viral impact. The client estimated 4 weeks and 200 hours of work: We wrapped up the app and mobile site in 10 days and under budget.'),
-	(2,1,'#LOVECONQUERSHATE','hrc','Human Rights Campaign','For VFW we were tasked with creating a microsite fundraiser and mobile application that would be executed over Facebook messaging to spread the viral impact. The client estimated 4 weeks and 200 hours of work: We wrapped up the app and mobile site in 10 days and under budget.'),
-	(3,1,'Ticket to Ride','wme','Warren Miller Entertainment','Ticket to Ride presale is a facebook embedded app that we were asked to finish in approximately a week. Users can login, check showtimes, buy tickets, and share their information with friends. A full CMS backend was included for admins to update and manage shows.  '),
-	(4,1,'Hennepin ','wf','Whole Foods','Whole Foods Hennepin opening day promo giveaway is a simple facebook embedded questionnaire and mailing list application, with minor javascript logic.  We were asked to complete due to lack of resources from the client. '),
-	(5,1,'Beat The Leader','oc','Old Chicago','While based on a previous app Black and Red Inc. helped to create, it was necessary to re-purpose and overhaul the feature set for this page. We had to re-purpose and re-skin the codebase in just under 4 days to get this up and running under deadline!'),
-	(6,1,'NFL Pick\'em Rams','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. '),
-	(7,1,'NFL Pick\'em Bears','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. '),
-	(8,1,'NFL Pick\'em Lions','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. '),
-	(9,2,'Monthly Updates','ad','Aspen Dental','When the client lost resources for this Aspen Dental site update, Black & Red was brought on to finish it. Adding to the complexity, the site was geo-enabled from over 500 different locations to allow users to see local phone numbers and schedule appointments. '),
-	(10,2,'FB Page','ml','MetLife','Metlife needed a response form page for their "Request a Free Quote" Facebook app. Black & Red built a one-page, responsively designed form and submission component for the customer to generate quotes. QA was also overseen by Black & Red as the project went live for Metlife. '),
-	(11,2,'Jello Recipes','krj','Kraft','Black & Red came on to this project with JELLO to update the website for spring and summer sales campaigns. Other work included a featured product carousel, embedded YouTube player, modified grid layour, and Instagram tile that links out to JELL-O account on Instagram. '),
-	(12,2,'C3 Updates','apb','Apple Bee\'s','Black & Red was assigned to help update several menu pages for the Applebees.com desktop page. Updated menus for the summer were added, as well as active location tools that would help the user find the closest restaurant. '),
-	(13,2,'Homepage Updates','fotl','Fruit Of The Loom','Black & Red was brought on to handle multiple maintenance issues and updates on the Fruit of the Loom desktop page.  Updates were made on the desktop and mobile versions, including ties fo the admin CMS. '),
-	(14,2,'Naked Cowboy','fotl','Fruit Of The Loom','Fruit of the Loom needed a new homepage layout built, as well as several backend updates. We created an image carousel, admin CMS image control was built in, a code refactor was done for a new PHP based distribution on the backend, and an image based twitter feed was built.'),
-	(15,2,'Dadisms','fotl','Fruit Of The Loom','For Father\'s Day, Fruit of the Loom needed desktop and mobile versions their Dadisms campaign built. Dadisms allowed users to buy custom pairs of Father\'s Day underwear. An image carousel, inventory control, social media hooks, and inventory countdown ticker were all utilized.'),
-	(16,2,'Gif Registry','fotl','Fruit Of The Loom','As a Father\'s Day promotion, Black & Red built the Gif Registry project for desktop and mobile. A Gif carousel was built to display rotating animated gifs on the main page. Animated GIF vouchers could be emailed to Dad for Father\'s Day, and social media components were built in as well.'),
-	(17,3,'Hometown Hills','wme','Warren Miller Entertainment','Warren Miller Entertainment brought on Black & Red to develop a tournament-style, bracketed video competition Facebook app. Users uploaded amatuer videos, which were automatically paired. Votes were tallied, and the competitors were narrowed to a final four and championship voting round.');
+	(1,1,'Mane Event','vfw','VFW','For VFW we were tasked with creating a microsite fundraiser and mobile application that would be executed over Facebook messaging to spread the viral impact. The client estimated 4 weeks and 200 hours of work: We wrapped up the app and mobile site in 10 days and under budget.',1),
+	(2,1,'#LOVECONQUERSHATE','hrc','Human Rights Campaign','For VFW we were tasked with creating a microsite fundraiser and mobile application that would be executed over Facebook messaging to spread the viral impact. The client estimated 4 weeks and 200 hours of work: We wrapped up the app and mobile site in 10 days and under budget.',1),
+	(3,1,'Ticket to Ride','wme','Warren Miller Entertainment','Ticket to Ride presale is a facebook embedded app that we were asked to finish in approximately a week. Users can login, check showtimes, buy tickets, and share their information with friends. A full CMS backend was included for admins to update and manage shows.  ',1),
+	(4,1,'Hennepin ','wf','Whole Foods','Whole Foods Hennepin opening day promo giveaway is a simple facebook embedded questionnaire and mailing list application, with minor javascript logic.  We were asked to complete due to lack of resources from the client. ',0),
+	(5,1,'Beat The Leader','oc','Old Chicago','While based on a previous app Black and Red Inc. helped to create, it was necessary to re-purpose and overhaul the feature set for this page. We had to re-purpose and re-skin the codebase in just under 4 days to get this up and running under deadline!',0),
+	(6,1,'Pick\'em Rams','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. ',1),
+	(7,1,'Pick\'em Bears','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. ',1),
+	(8,1,'Pick\'em Lions','nfl','NFL','The NFL pick?em apps were highly social facebook integrated fantasy football apps. The apps allow players to challenge their friends and see who can predict their favorite team?s winnings off live data queried from the NFL scraped homepage. Text and email notifications were built in. ',1),
+	(9,2,'Monthly Updates','ad','Aspen Dental','When the client lost resources for this Aspen Dental site update, Black & Red was brought on to finish it. Adding to the complexity, the site was geo-enabled from over 500 different locations to allow users to see local phone numbers and schedule appointments. ',0),
+	(10,2,'FB Page','ml','MetLife','Metlife needed a response form page for their "Request a Free Quote" Facebook app. Black & Red built a one-page, responsively designed form and submission component for the customer to generate quotes. QA was also overseen by Black & Red as the project went live for Metlife. ',0),
+	(11,2,'Jello Recipes','krj','Kraft','Black & Red came on to this project with JELLO to update the website for spring and summer sales campaigns. Other work included a featured product carousel, embedded YouTube player, modified grid layour, and Instagram tile that links out to JELL-O account on Instagram. ',0),
+	(12,2,'C3 Updates','apb','Applebee\'s','Black & Red was assigned to help update several menu pages for the Applebees.com desktop page. Updated menus for the summer were added, as well as active location tools that would help the user find the closest restaurant. ',0),
+	(13,2,'Homepage Updates','fotl','Fruit Of The Loom','Black & Red was brought on to handle multiple maintenance issues and updates on the Fruit of the Loom desktop page.  Updates were made on the desktop and mobile versions, including ties fo the admin CMS. ',1),
+	(14,2,'Naked Cowboy','fotl','Fruit Of The Loom','Fruit of the Loom needed a new homepage layout built, as well as several backend updates. We created an image carousel, admin CMS image control was built in, a code refactor was done for a new PHP based distribution on the backend, and an image based twitter feed was built.',1),
+	(15,2,'Dadisms','fotl','Fruit Of The Loom','For Father\'s Day, Fruit of the Loom needed desktop and mobile versions their Dadisms campaign built. Dadisms allowed users to buy custom pairs of Father\'s Day underwear. An image carousel, inventory control, social media hooks, and inventory countdown ticker were all utilized.',1),
+	(16,2,'Gif Registry','fotl','Fruit Of The Loom','As a Father\'s Day promotion, Black & Red built the Gif Registry project for desktop and mobile. A Gif carousel was built to display rotating animated gifs on the main page. Animated GIF vouchers could be emailed to Dad for Father\'s Day, and social media components were built in as well.',1),
+	(17,3,'Hometown Hills','wme','Warren Miller Entertainment','Warren Miller Entertainment brought on Black & Red to develop a tournament-style, bracketed video competition Facebook app. Users uploaded amatuer videos, which were automatically paired. Votes were tallied, and the competitors were narrowed to a final four and championship voting round.',1);
 
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
