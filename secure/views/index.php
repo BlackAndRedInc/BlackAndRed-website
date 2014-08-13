dcdd34ac903f77974aabb04a8a1390f09a20aa72
Phillip Epstein <Phillip@blackandred.io>
2014-08-13 08:59:44 -0600

    Added GA code

--- secure/views/index.php
+++ secure/views/index.php
@@ -109,13 +109,13 @@ require_once 'clients.model.php';
 
 
 		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-		<script type="text/javascript">
+		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-47452134-1', 'blackandredconsulting.com');
+			ga('create', 'UA-53779333-1', 'auto');
 			ga('send', 'pageview');
 		</script>
 
