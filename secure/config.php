6cd14f3c9c847bea42451322d1f98b5f99045c14
Phillip Epstein <Phillip@core-tech.com>
2015-01-13 05:45:52 -0700

    Fixed config

--- secure/config.php
+++ secure/config.php
@@ -2,9 +2,9 @@
 //////////////////////////////////
 /* Config  Stuff */
 //////////////////////////////////
-define("DATABASE_SERVER",	isset($_ENV["DATABASE1_HOST"]) ? $_ENV["DB1_HOST"]	: 'localhost');
-define("DATABASE_PORT",		isset($_ENV["DATABASE1_PORT"]) ? $_ENV["DB1_PORT"]	: 3306);
-define("DATABASE_USER",		isset($_ENV["DATABASE1_USER"]) ? $_ENV["DB1_USER"]    : 'root');
-define("DATABASE_PASS",		isset($_ENV["DATABASE1_PASS"]) ? $_ENV["DB1_PASS"]	: 'root');
-define("DATABASE_NAME",		isset($_ENV["DATABASE1_NAME"]) ? $_ENV["DB1_NAME"]	: 'blackAndRed');
+define("DATABASE_SERVER",	isset($_ENV["DATABASE1_HOST"]) ? $_ENV["DATABASE1_HOST"]	: 'localhost');
+define("DATABASE_PORT",		isset($_ENV["DATABASE1_PORT"]) ? $_ENV["DATABASE1_PORT"]	: 3306);
+define("DATABASE_USER",		isset($_ENV["DATABASE1_USER"]) ? $_ENV["DATABASE1_USER"]    : 'root');
+define("DATABASE_PASS",		isset($_ENV["DATABASE1_PASS"]) ? $_ENV["DATABASE1_PASS"]	: 'root');
+define("DATABASE_NAME",		isset($_ENV["DATABASE1_NAME"]) ? $_ENV["DATABASE1_NAME"]	: 'blackAndRed');
 define("CRYPT_SALT", 'X6jmeZffSv473eu8GhdV');
